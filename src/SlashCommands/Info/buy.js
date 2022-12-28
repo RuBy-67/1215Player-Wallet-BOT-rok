c3d0bcf60e2f517bb24dc86a9feb080a0bd24871
RuBy-67 <beyer.ruben67@gmail.com>
2022-12-28 01:02:35 +0100

    modified:   .gitignore 	new file:   src/SlashCommands/Info/buy.js 	modified:   src/SlashCommands/Info/help.js 	new file:   src/SlashCommands/Sheets/addmin.js 	modified:   src/SlashCommands/Sheets/check.js 	new file:   src/SlashCommands/Sheets/user.js

--- src/SlashCommands/Info/buy.js
+++ src/SlashCommands/Info/buy.js
@@ -1,24 +1,16 @@
 const { MessageEmbed } = require("discord.js");
 
 module.exports = {
-  name: "help",
-  description: "Shows the help page!",
+  name: "buy",
+  description: "Shop of 1215 Kingdom",
   options: null,
   run: async (client, interaction, args) => {
     const embed = new MessageEmbed()
-      .setTitle("Help Menu")
-      .setDescription(
-        client.slash
-          .map((cmd) => `\`${cmd.name} - ${cmd.description}\``)
-          .join("\n")
-      )
-      .addField(
-        "Sheet Link",
-        `[Google_Sheet-Temporaire-](https://docs.google.com/spreadsheets/d/1Dd3zH9E_GxD6IOIaeiql6cnsOw1p6Mkydz35xvFE09w/edit?usp=sharing)`
-      )
+      .setTitle("1215 Shopping ")
+      .setDescription("")
       .setColor("GREEN")
       .setFooter(
-        "1215",
+        "TEST",
         "https://media.discordapp.net/attachments/1057030746105200650/1057034989918761041/DALLE_2022-12-15_21.11.27_-_digital_art_of_pineaple_with_solar_glass.png?width=905&height=905"
       );
 
---
+++ src/SlashCommands/Sheets/user.js
@@ -0,0 +1,79 @@
+const { MessageEmbed } = require("discord.js");
+const emo = require(`../../jsons/emoji.json`);
+
+module.exports = {
+  name: "user",
+  description: "Checks if you are in the list",
+  options: [
+    {
+      name: "user",
+      description: "The user to check",
+      type: "USER",
+      required: true,
+    },
+  ],
+
+  run: async (client, interaction, args) => {
+    function emoji(id) {
+      return client.emojis.cache.get(id).toString();
+    }
+    const user = await interaction.options.getUser("user");
+    const username = await user.username;
+
+    const rows = await client.googleSheets.values.get({
+      auth: client.auth,
+      spreadsheetId: client.sheetId,
+      range: "Sheet1!A:G",
+    });
+
+    const data = rows.data.values.find((row) => row[0] === username);
+
+    if (data) {
+      if (rows.data.values.length > 0) {
+        const embed = new MessageEmbed().setColor("GREEN");
+
+        for (let i = 0; i < rows.data.values.length; i++) {
+          const row = rows.data.values[i];
+          if (row[0] === username) {
+            embed.setTitle(`${row[0]} - ${row[2]}`);
+            embed.setThumbnail(client.user.displayAvatarURL());
+            embed.addFields(
+              {
+                name: emoji(emo.books) + " In Game Info :",
+                value: `__InGame Name :__ ${row[2]}\n __Rok Id:__ ${row[3]}`,
+                inline: false,
+              },
+              {
+                name: emoji(emo.lohar2) + "Current Power :",
+                value: `${row[4]}` + emoji(emo.star),
+                inline: true,
+              },
+              {
+                name: emoji(emo.sword) + " Current Kill Power :",
+                value: `${row[5]} ` + emoji(emo.skull),
+                inline: true,
+              },
+              {
+                name: emoji(emo.bagGems) + " Current Credit :",
+                value: `${row[6]} ` + emoji(emo.gems),
+                inline: false,
+              }
+            );
+
+            embed.setTimestamp(Date.now());
+            embed.setFooter(
+              `${row[2]}`,
+              "https://media.discordapp.net/attachments/1057030746105200650/1057034989918761041/DALLE_2022-12-15_21.11.27_-_digital_art_of_pineaple_with_solar_glass.png?width=905&height=905"
+            );
+          }
+        }
+
+        return interaction.reply({ embeds: [embed] });
+      }
+    } else if (!data) {
+      return interaction.reply(
+        "User is not in the list! for add you type `/add`"
+      );
+    }
+  },
+};
