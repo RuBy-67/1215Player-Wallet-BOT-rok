8d485145d7e2c3d7b9d682a27f7a13e76e30a545
RuBy-67 <beyer.ruben67@gmail.com>
2022-12-31 12:26:37 +0100

    modified:   src/SlashCommands/Sheets/update.js

--- src/SlashCommands/Sheets/update.js
+++ src/SlashCommands/Sheets/update.js
@@ -3,9 +3,31 @@ module.exports = {
   description: "Adds a user to the spreadsheet!",
   options: [
     {
+      name: "type",
+      description: "Main or Farm Account",
+      required: true,
+      type: "STRING",
+      choices: [
+        {
+          name: "main_account",
+          value: "main_account",
+        },
+        {
+          name: "farm_account",
+          value: "farm_account",
+        },
+      ],
+    },
+    {
       name: "user",
       description: "The user to add to the spreadsheet",
       type: "USER",
+      required: true,
+    },
+    {
+      name: "rokid",
+      description: "in Game Id of player",
+      type: "STRING",
       required: true,
     },
     {
@@ -17,12 +39,6 @@ module.exports = {
     {
       name: "power",
       description: "Current power of the player",
-      type: "STRING",
-      required: true,
-    },
-    {
-      name: "rokid",
-      description: "in Game Id of player",
       type: "STRING",
       required: false,
     },
@@ -39,36 +55,57 @@ module.exports = {
     const IgId = await interaction.options.getString("rokid");
     const power = await interaction.options.getString("power");
     const Kpower = await interaction.options.getString("killpower");
+    const choices = await interaction.options.getString("type");
     const id = await user.id;
     const username = await user.username;
 
-    const rows = await client.googleSheets.values.get({
-      auth: client.auth,
-      spreadsheetId: client.sheetId,
-      range: "Sheet1!A:A",
-    });
+    if (choices === "main_account") {
+      const rows = await client.googleSheets.values.get({
+        auth: client.auth,
+        spreadsheetId: client.sheetId,
+        range: "Sheet1!A:A",
+      });
 
-    const data = rows.data.values;
+      const data = rows.data.values;
+      const rowIndex = data.findIndex((row) => row[0] === id);
+      if (rowIndex == -1) {
+        return interaction.reply("User not found");
+      }
+      const range1 = `Sheet1!A${rowIndex + 1}:Z${rowIndex + 1}`;
+      await client.googleSheets.values.update({
+        auth: client.auth,
+        spreadsheetId: client.sheetId,
+        range: range1,
+        valueInputOption: "USER_ENTERED",
+        resource: {
+          values: [[id, username, IgN, IgId, power, Kpower]],
+        },
+      });
+      return interaction.reply("User has been updated successfully !`");
+    } else if (choices === "farm_account") {
+      const rows2 = await client.googleSheets.values.get({
+        auth: client.auth,
+        spreadsheetId: client.sheetId,
+        range: "Sheet2!D:D",
+      });
 
-    const rowIndex = data.findIndex((row) => row[0] === id);
-    const Index = data.findIndex((row) => row[3] === IgId);
-    console.log(rowIndex);
-    if (rowIndex === -1) {
-      return interaction.reply("User not found");
+      const data2 = rows2.data.values;
+
+      const Index = data2.findIndex((row) => row[0] === IgId);
+      if (Index == -1) {
+        return interaction.reply("User not found");
+      }
+      const range2 = `Sheet2!A${Index + 1}:F${Index + 1}`;
+      await client.googleSheets.values.update({
+        auth: client.auth,
+        spreadsheetId: client.sheetId,
+        range: range2,
+        valueInputOption: "USER_ENTERED",
+        resource: {
+          values: [[id, username, IgN, IgId, power, Kpower]],
+        },
+      });
+      return interaction.reply(":corn: Farm has been updated successfully !`");
     }
-    if (Index === 1) {
-      return interaction.reply(" found");
-    }
-    const range = `A${rowIndex + 1}:Z${rowIndex + 1}`;
-    await client.googleSheets.values.update({
-      auth: client.auth,
-      spreadsheetId: client.sheetId,
-      range,
-      valueInputOption: "USER_ENTERED",
-      resource: {
-        values: [[id, username, IgN, IgId, power, Kpower]],
-      },
-    });
-    return interaction.reply("User has been updated successfully !`");
   },
 };
