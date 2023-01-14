e0d03d7f496dbf13e90c193797ca46e18569202a
RuBy-67 <beyer.ruben67@gmail.com>
2023-01-15 00:42:57 +0100

    modified:   src/SlashCommands/Info/buy.js

--- src/SlashCommands/Info/buy.js
+++ src/SlashCommands/Info/buy.js
@@ -7,14 +7,13 @@ module.exports = {
   run: async (client, interaction, args) => {
     const embed = new MessageEmbed()
       .setTitle("1215 Shopping ")
-      .setDescription("")
+      .setDescription("🛠️")
       .setColor("GREEN")
       .setFooter({
         text: "1215",
         iconURL:
           "https://media.discordapp.net/attachments/1057030746105200650/1057034989918761041/DALLE_2022-12-15_21.11.27_-_digital_art_of_pineaple_with_solar_glass.png?width=905&height=90",
       });
-      
 
     return interaction.reply({ embeds: [embed] });
   },
