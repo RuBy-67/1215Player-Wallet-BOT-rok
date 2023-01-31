888b8c06ba6925a38c36550444e44d3d87df58e2
RuBy-67 <beyer.ruben67@gmail.com>
2023-01-31 15:08:40 +0100

    modified:   src/SlashCommands/Tips/tipleader.js

--- src/SlashCommands/Tips/tipleader.js
+++ src/SlashCommands/Tips/tipleader.js
@@ -99,44 +99,47 @@ module.exports = {
         "https://media.discordapp.net/attachments/1057030746105200650/1057034989918761041/DALLE_2022-12-15_21.11.27_-_digital_art_of_pineaple_with_solar_glass.png?width=905&height=905",
     });
 
-    if (com == "ysg") {
+    if (com == "Aethelflaed") {
       embed.setColor("#FFDC00");
-      embed.setTitle("Yi-Seong-Gye");
-      embed.setURL("https://www.rok.guide/commanders/yi-seong-gye/");
+      embed.setTitle("Æthelflæd");
+      embed.setURL("https://www.rok.guide/aethelflaed/");
       embed.setThumbnail(
-        "https://cdn.discordapp.com/attachments/1069360006908084245/1069378929900662784/yi-seong-gye-225x300.png"
+        "https://cdn.discordapp.com/attachments/1069360006908084245/1069976313881964604/Aethelflaed_full-1-530x630.png"
       );
       embed.setImage(
-        "https://cdn.discordapp.com/attachments/1069360006908084245/1069379608094457947/Yi-Seong-Gye-Open-Field-Build-scaled.png"
+        "https://cdn.discordapp.com/attachments/1069720356379709595/1069978222810378290/aethelflaed-mixed-troops-talent-tree.png"
       );
       embed.setDescription(
-        `Yi Seong-Gye (YSG) is a Legendary Archer commander, one of the best commanders in Rise of Kingdoms. When choosing any Archer Civilization as a special unit, Yi Seong-Gye must be our first Legendary to reach maximum levels and skills, even playing with Cavalry or Infantry it will never stop being significant at any point in the game.\n\n__Best Pairing :__ \n` +
-          emoji(emo.EdwardofWoodstock) +
-          emoji(emo.RamessesII1) +
-          emoji(emo.ElCid) +
-          emoji(emo.Tomyris) +
-          emoji(emo.CharlesMartel) +
-          emoji(emo.RichardI) +
-          emoji(emo.WuZetian) +
+        `Aethelflaed is by far the most useful commander in every aspect of Rise of Kingdoms. A lot of people will say that Aethelflaed is there for free to play players but that is not true. Most of the pay to win players will use her in fights because of the first skill.\n\n__Best Pairing Primary:__ \n` +
+          emoji(emo.MehmedII) +
+          emoji(emo.JoanofArc1) +
+          emoji(emo.YiSeongGye) +
+          emoji(emo.OsmanI) +
           emoji(emo.SunTzu) +
-          emoji(emo.Hermann)
+          emoji(emo.FrederickI) +
+          `\n\n__Best Pairing Secondary:__ \n` +
+          emoji(emo.RichardI) +
+          emoji(emo.Baibars) +
+          emoji(emo.AlexandertheGreat) +
+          emoji(emo.Belisarius) +
+          `__Video Guide :__\n>>> [Guide Free commander](https://youtu.be/FrT0Kao_p-A)`
       );
       embed.setAuthor({
         name: "rok-guide",
         iconURL:
-          "https://cdn.discordapp.com/attachments/1069360006908084245/1069360099761594449/Yi-Seong-Gye.png",
+          "https://cdn.discordapp.com/attachments/1069360006908084245/1069360449805631639/Aethelflaed.png",
       });
       embed.setFields(
         {
           name: "Specialties :",
           value:
             "\u200B" +
-            emoji(emo.archer) +
-            `**|** *Archery* ` +
-            emoji(emo.garrison) +
-            `**|** *Garrison* ` +
-            emoji(emo.skill) +
-            `**|** *Skill*`,
+            emoji(emo.leadership) +
+            `**|** *Leadership* ` +
+            emoji(emo.peacekeeping) +
+            `**|** *Peacekeeper* ` +
+            emoji(emo.support) +
+            `**|** *Support*`,
         },
         {
           name: "Overall :",
@@ -145,8 +148,8 @@ module.exports = {
             emoji(emo.etoile_plein) +
             emoji(emo.etoile_plein) +
             emoji(emo.etoile_plein) +
-            emoji(emo.etoile_plein) +
-            emoji(emo.etoile_plein) +
+            emoji(emo.etoile_vide) +
+            emoji(emo.etoile_vide) +
             "*",
           inline: true,
         },
@@ -167,10 +170,222 @@ module.exports = {
           value:
             "*" +
             emoji(emo.etoile_plein) +
+            emoji(emo.etoile_vide) +
+            emoji(emo.etoile_vide) +
+            emoji(emo.etoile_vide) +
+            emoji(emo.etoile_vide) +
+            "*",
+          inline: true,
+        },
+        {
+          name: "Rallying Object :",
+          value:
+            "*" +
+            emoji(emo.etoile_plein) +
+            emoji(emo.etoile_vide) +
+            emoji(emo.etoile_vide) +
+            emoji(emo.etoile_vide) +
+            emoji(emo.etoile_vide) +
+            "*",
+          inline: true,
+        },
+        {
+          name: "Defending Cities :",
+          value:
+            "*" +
+            emoji(emo.etoile_plein) +
+            emoji(emo.etoile_vide) +
+            emoji(emo.etoile_vide) +
+            emoji(emo.etoile_vide) +
+            emoji(emo.etoile_vide) +
+            "*",
+          inline: true,
+        },
+        {
+          name: "Defending Object :",
+          value:
+            "*" +
+            emoji(emo.etoile_plein) +
+            emoji(emo.etoile_vide) +
+            emoji(emo.etoile_vide) +
+            emoji(emo.etoile_vide) +
+            emoji(emo.etoile_vide) +
+            "*",
+          inline: true,
+        }
+      );
+      return interaction.reply({ embeds: [embed] });
+    } else if (com == "charlemagne") {
+      embed.setColor("#FFDC00");
+      embed.setTitle("Charlemagne");
+      embed.setURL("https://www.rok.guide/Charlemagne/");
+      embed.setThumbnail(
+        "https://cdn.discordapp.com/attachments/1069360006908084245/1069979789458419722/Charlemagne_full-1-397x630.png"
+      );
+      embed.setImage(
+        "https://cdn.discordapp.com/attachments/1069720356379709595/1069980050683875458/charlemagne-rally-mixed-troops.png"
+      );
+      embed.setDescription(
+        `Charlemagne is a legendary commander that was part of a reward in the first KVK in Rise of Kingdoms. The only purpose of Charlemagne is to rally city. Unfortunately, Charlemagne by far one of the worst legendary commanders in Rise Of kingdoms.\n\n__Best Pairing :__ \n` +
+          emoji(emo.JuliusCaesar) +
+          emoji(emo.HannibalBarca) 
+      );
+      embed.setAuthor({
+        name: "rok-guide",
+        iconURL:
+          "https://cdn.discordapp.com/attachments/1069360006908084245/1069360411276750878/Charlemagne.png",
+      });
+      embed.setFields(
+        {
+          name: "Specialties :",
+          value:
+            "\u200B" +
+            emoji(emo.leadership) +
+            `**|** *Leadership* ` +
+            emoji(emo.conquering) +
+            `**|** *Conquering* ` +
+            emoji(emo.skill) +
+            `**|** *Skill*`,
+        },
+        {
+          name: "Overall :",
+          value:
+            "*" +
+            emoji(emo.etoile_plein) +
+            emoji(emo.etoile_plein) +
+            emoji(emo.etoile_plein) +
+            emoji(emo.etoile_vide) +
+            emoji(emo.etoile_vide) +
+            "*",
+          inline: true,
+        },
+        {
+          name: "Open Field :",
+          value:
+            "*" +
+            emoji(emo.etoile_plein) +
+            emoji(emo.etoile_vide) +
+            emoji(emo.etoile_vide) +
+            emoji(emo.etoile_vide) +
+            emoji(emo.etoile_vide) +
+            "*",
+          inline: true,
+        },
+        {
+          name: "Rallying Cities :",
+          value:
+            "*" +
+            emoji(emo.etoile_plein) +
+            emoji(emo.etoile_plein) +
+            emoji(emo.etoile_plein) +
+            emoji(emo.etoile_vide) +
+            emoji(emo.etoile_vide) +
+            "*",
+          inline: true,
+        },
+        {
+          name: "Rallying Object :",
+          value:
+            "*" +
+            emoji(emo.etoile_plein) +
+            emoji(emo.etoile_plein) +
+            emoji(emo.etoile_plein) +
+            emoji(emo.etoile_vide) +
+            emoji(emo.etoile_vide) +
+            "*",
+          inline: true,
+        },
+        {
+          name: "Defending Cities :",
+          value:
+            "*" +
+            emoji(emo.etoile_plein) +
+            emoji(emo.etoile_vide) +
+            emoji(emo.etoile_vide) +
+            emoji(emo.etoile_vide) +
+            emoji(emo.etoile_vide) +
+            "*",
+          inline: true,
+        },
+        {
+          name: "Defending Object :",
+          value:
+            "*" +
+            emoji(emo.etoile_plein) +
+            emoji(emo.etoile_vide) +
+            emoji(emo.etoile_vide) +
+            emoji(emo.etoile_vide) +
+            emoji(emo.etoile_vide) +
+            "*",
+          inline: true,
+        }
+      );
+      return interaction.reply({ embeds: [embed] });
+    } else if (com == "frederick") {
+      embed.setColor("#FFDC00");
+      embed.setTitle("Frederick I");
+      embed.setURL("https://www.rok.guide/commanders/frederick-i/");
+      embed.setThumbnail(
+        "https://cdn.discordapp.com/attachments/1069360006908084245/1069981754816995378/frederick-1-1-228x300.png"
+      );
+      embed.setImage(
+        "https://cdn.discordapp.com/attachments/1069720356379709595/1069982178060021820/Frederick-I-Rally-Build-Mixed-Troops.png"
+      );
+      embed.setDescription(
+        `Frederick I is one of the first legendary commanders in Rise of Kingdoms. He was great when Rise Of Kingdoms was released but now Frederick is not so good. Do not invest universal legendary sculptures in Frederick, it is not worth it now and you can get him gold chests.\n\n__Best Pairing :__ \n` +
+          emoji(emo.JuliusCaesar) +
+          emoji(emo.HannibalBarca)
+      );
+      embed.setAuthor({
+        name: "rok-guide",
+        iconURL:
+          "https://cdn.discordapp.com/attachments/1069360006908084245/1069360342565670972/Frederick-I.png",
+      });
+      embed.setFields(
+        {
+          name: "Specialties :",
+          value:
+            "\u200B" +
+            emoji(emo.leadership) +
+            `**|** *Leadership* ` +
+            emoji(emo.conquering) +
+            `**|** *Conquering* ` +
+            emoji(emo.skill) +
+            `**|** *Skill*`,
+        },
+        {
+          name: "Overall :",
+          value:
+            "*" +
             emoji(emo.etoile_plein) +
             emoji(emo.etoile_plein) +
             emoji(emo.etoile_plein) +
             emoji(emo.etoile_plein) +
+            emoji(emo.etoile_vide) +
+            "*",
+          inline: true,
+        },
+        {
+          name: "Open Field :",
+          value:
+            "*" +
+            emoji(emo.etoile_plein) +
+            emoji(emo.etoile_plein) +
+            emoji(emo.etoile_plein) +
+            emoji(emo.etoile_plein) +
+            emoji(emo.etoile_vide) +
+            "*",
+          inline: true,
+        },
+        {
+          name: "Rallying Cities :",
+          value:
+            "*" +
+            emoji(emo.etoile_plein) +
+            emoji(emo.etoile_plein) +
+            emoji(emo.etoile_plein) +
+            emoji(emo.etoile_plein) +
+            emoji(emo.etoile_vide) +
             "*",
           inline: true,
         },
@@ -182,7 +397,7 @@ module.exports = {
             emoji(emo.etoile_plein) +
             emoji(emo.etoile_plein) +
             emoji(emo.etoile_plein) +
-            emoji(emo.etoile_plein) +
+            emoji(emo.etoile_vide) +
             "*",
           inline: true,
         },
@@ -192,9 +407,9 @@ module.exports = {
             "*" +
             emoji(emo.etoile_plein) +
             emoji(emo.etoile_plein) +
-            emoji(emo.etoile_plein) +
-            emoji(emo.etoile_plein) +
-            emoji(emo.etoile_plein) +
+            emoji(emo.etoile_vide) +
+            emoji(emo.etoile_vide) +
+            emoji(emo.etoile_vide) +
             "*",
           inline: true,
         },
@@ -204,19 +419,13 @@ module.exports = {
             "*" +
             emoji(emo.etoile_plein) +
             emoji(emo.etoile_plein) +
-            emoji(emo.etoile_plein) +
-            emoji(emo.etoile_plein) +
-            emoji(emo.etoile_plein) +
+            emoji(emo.etoile_vide) +
+            emoji(emo.etoile_vide) +
+            emoji(emo.etoile_vide) +
             "*",
           inline: true,
         }
       );
-      return interaction.reply({ embeds: [embed] });
-    } else if (com == "ramesses") {
-      embed.setColor("#FFDC00");
-      return interaction.reply({ embeds: [embed] });
-    } else if (com == "elcid") {
-      embed.setColor("#FFDC00");
       return interaction.reply({ embeds: [embed] });
     } else if (com == "eow") {
       embed.setColor("#FFDC00");
